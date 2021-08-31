83d4fd75a0cb332e844086504ad9a8541c2bc637
Katherine Blasman <b.katherine@hotmail.com>
2021-08-31 14:26:05 -0400

    adding dropdown toggle menu

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
 window.onload = function() {
     document.querySelector('.preloader').style.display = "none"
 }
+
+// ------------------ TOGGLE FUNCTION ---------------------
+// dropdown menu toggle -----------------------------------
+document.querySelector('.menu-toggle').addEventListener('click', openMenu);
+
+function openMenu() {
+    document.querySelector('.menu').classList.toggle("active");
+}
